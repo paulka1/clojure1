c3802be19e26a23134941fbdd7b5c2f48941c21d
o0freedom0o <luantranthanh24021995@gmail.com>
2021-07-01 18:51:39 +0200

    update 1

--- src/tictactoe/core.cljs
+++ src/tictactoe/core.cljs
@@ -12,22 +12,36 @@
 (defn make-morpion []
   (vec (take 3 (repeat 0))))
 
+(defn new-board [n]
+  (vec (repeat n (vec (repeat n 0)))))
+
 (defonce app-state (atom {:text "TIC TAC TOE!"
-                          :board (make-morpion)}))
+                          :board (new-board 3)}))
 
 
 (defn tictactoe []
       [:div
        [:h1 (:text @app-state)]
-         [:svg
-          {:view-box "0 0 30 30"
-           :width 500
-           :height 500}
-          (for [i (range (count (:board @app-state)))
-                j (range (count (:board @app-state)))]
-            [:rect {:width 0.4
-                    :height 0.4
-                    :x i :y j}])]])
+       (into
+       [:svg
+        {:view-box "0 0 3 3"
+         :width 500
+         :height 500}
+        (for [i (range (count (:board @app-state)))
+              j (range (count (:board @app-state)))]
+          [:rect {:width 0.9
+                  :height 0.9
+                  :fill (if (zero? (get-in @app-state [:board j i]))
+                          "red"
+                          "blue")
+                  :x i
+                  :y j
+                  :on-click
+                  (fn rect-click [e]
+                    (prn "you clicked me!" i j)
+                    (prn (:board @app-state))
+                    (prn (swap! app-state assoc-in [:board j i] 1 )))
+                    }])])])
 
 (rd/render [tictactoe]
            (. js/document (getElementById "app")))
