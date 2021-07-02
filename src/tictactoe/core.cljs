9c3ef603ba75e6d0130d2ffca9ca833faa76417a
o0freedom0o <luantranthanh24021995@gmail.com>
2021-07-02 15:28:56 +0200

    update 1

--- src/tictactoe/core.cljs
+++ src/tictactoe/core.cljs
@@ -6,13 +6,57 @@
 (enable-console-print!)
 
 (defn make-morpion []
-  (vec (take 3 (repeat 0))))
+  (vec (take 9 (repeat 0))))
+
+;; ; -------------------------------------------
+;; ; Renvoie le symbole present dans  
+;; ; la case (x,y) du plateau
+;; ; x et y sont donnes en coordonnees de 0 a 2
+(defn get-case-morpion [plateau i j]
+  (get plateau (+ i (* 3 j))))
+
+; -------------------------------------------
+; Affiche le plateau
+
+(defn print-morpion [plateau]
+  (defn pcase [c]
+    (case c
+      0 "."
+      1 "X"
+      2 "O"))
+  (newline)
+  (dotimes [i 3]
+    (dotimes [j 3]
+      (print (pcase (get-case-morpion plateau i j))))
+    (newline)))
+
+(defn set-case-morpion [plateau player i j]
+  (assoc plateau (+ i (* 3 j)) player))
+
+(defn exchange [player]
+  (- 3 player))
+
+(defn winning-morpion? [plateau]
+  (defn test [a b c] (and (not= a 0) (= a b) (= a c)))
+  (or (test (get-case-morpion plateau 0 0)  (get-case-morpion plateau 0 1)  (get-case-morpion plateau 0 2))
+      (test (get-case-morpion plateau 1 0)  (get-case-morpion plateau 1 1)  (get-case-morpion plateau 1 2))
+      (test (get-case-morpion plateau 2 0)  (get-case-morpion plateau 2 1)  (get-case-morpion plateau 2 2))
+
+      (test (get-case-morpion plateau 0 0)  (get-case-morpion plateau 1 0)  (get-case-morpion plateau 2 0))
+      (test (get-case-morpion plateau 0 1)  (get-case-morpion plateau 1 1)  (get-case-morpion plateau 2 1))
+      (test (get-case-morpion plateau 0 2)  (get-case-morpion plateau 1 2)  (get-case-morpion plateau 2 2))
+
+      (test (get-case-morpion plateau 0 0)  (get-case-morpion plateau 1 1)  (get-case-morpion plateau 2 2))
+      (test (get-case-morpion plateau 2 0)  (get-case-morpion plateau 1 1)  (get-case-morpion plateau 0 2))))
 
 (defn new-board [n]
   (vec (repeat n (vec (repeat n 0)))))
 
 (defonce app-state (atom {:text "TIC TAC TOE"
-                          :board (new-board 3)}))
+                          :board (new-board 3)
+                          :test (make-morpion)
+                          :player 1}))
+
 
 ;(defn computer-move []
 ;      (swap! app-state assoc-in [:board @app-state]
@@ -40,9 +84,12 @@
         :y (+ 0.05 j)
         :on-click
         (fn rect-click [e]
-            (swap! app-state update-in [:board j i]inc)
+          (swap! app-state update-in [:board j i] inc)
+          (swap! app-state assoc-in [:test] (set-case-morpion (:test @app-state) (:player @app-state) i j))
+          (swap! app-state assoc-in [:player] (exchange (:player @app-state)))
+          (print-morpion (:test @app-state))
             ;(computer-move)
-            )}])
+          )}])
 
 (defn circle [i j]
       [:circle
@@ -71,9 +118,9 @@
         {:view-box "0 0 3 3"
          :width 500
          :height 500}]
-        (for [i (range (count (:board @app-state)))
-              j (range (count (:board @app-state)))]
-             (case (get-in @app-state [:board j i])
+        (for [i [0 1 2]
+              j [0 1 2]]
+             (case (get-case-morpion (:test @app-state) i j)
                    0 [blank i j]
                    1 [circle i j]
                    2 [cross i j])))
@@ -82,7 +129,7 @@
          {
           :on-click
           (fn new-game-click [e]
-              (swap! app-state assoc :board (new-board 3)))
+              (swap! app-state assoc :board (make-morpion)))
           } "NEW GAME"]]])
 
 (rd/render [tictactoe]
